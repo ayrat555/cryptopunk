3abaa9826fafdd62678f4b65f839eec18af42127
Ayrat Badykov <ayratin555@gmail.com>
2021-10-30 12:56:35 +0300

    update props to publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Cryptopunk.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Placeholder for now",
+      package: [
+        maintainers: ["Ayrat Badykov"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/ayrat555/cryptopunk"}
+      ]
     ]
   end
 
@@ -21,6 +27,7 @@ defmodule Cryptopunk.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
