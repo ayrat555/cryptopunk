b4c45ab0a837abaf834bd67c8f18e3b0f291ff0b
Ayrat Badykov <ayratin555@gmail.com>
2021-12-27 18:38:43 +0300

    add additional seed test

--- test/cryptopunk/seed_test.exs
+++ test/cryptopunk/seed_test.exs
@@ -2,6 +2,7 @@ defmodule Cryptopunk.SeedTest do
   use ExUnit.Case
 
   alias Cryptopunk.Seed
+  alias Cryptopunk.Mnemonic
 
   describe "create/1" do
     test "creates seed from string" do
@@ -16,7 +17,7 @@ defmodule Cryptopunk.SeedTest do
       assert ExPBKDF2.verify(result, "hey", opts)
     end
 
-    test "consistency tests" do
+    test "Mastering bitcoin book tests" do
       tests = [
         {"5b56c417303faa3fcba7e57400e120a0ca83ec5a4fc9ffba757fbe63fbd77a89a1a3be4c67196f57c39a88b76373733891bfaba16ed27a813ceed498804c0570",
          ""},
@@ -32,6 +33,21 @@ defmodule Cryptopunk.SeedTest do
 
         assert expected_result == result
       end
+    end
+
+    test "Mastering bitcoin book test" do
+      expected_result =
+        "3269bce2674acbd188d4f120072b13b088a0ecf87c6e4cae41657a0bb78f5315b33b3a04356e53d062e55f1e0deaa082df8d487381379df848a6ad7e98798404"
+
+      entropy =
+        "2041546864449caff939d32d574753fe684d3c947c3346713dd8423e74abcf8c"
+        |> Base.decode16!(case: :lower)
+
+      mnemonic = Mnemonic.create_from_entropy(entropy)
+
+      result = mnemonic |> Seed.create() |> Base.encode16(case: :lower)
+
+      assert expected_result == result
     end
   end
 end
