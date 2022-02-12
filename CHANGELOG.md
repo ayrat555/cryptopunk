3c0ca10f97399863d03d4d53b2c4077607918c0c
Ayrat Badykov <ayratin555@gmail.com>
2022-02-12 07:50:41 +0200

    update nifs (#2)
    * update nifs
    
    * add pr link

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.3.1 (2022-02-12)
+
+- Update ex_pbkdf2 and ex_secp256k1 ([#2](https://github.com/ayrat555/cryptopunk/pull/2))
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cryptopunk.MixProject do
   def project do
     [
       app: :cryptopunk,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,8 +28,8 @@ defmodule Cryptopunk.MixProject do
   defp deps do
     [
       {:ex_keccak, "~> 0.3"},
-      {:ex_pbkdf2, "~> 0.3"},
-      {:ex_secp256k1, "~> 0.3"},
+      {:ex_pbkdf2, "~> 0.4"},
+      {:ex_secp256k1, "~> 0.4"},
       {:jason, "~> 1.3"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
