b3eb49895152f688eb9607561973146474bafe6c
Ayrat Badykov <ayratin555@gmail.com>
2022-04-03 14:33:57 +0300

    support tron addresses (#14)

--- lib/cryptopunk/crypto/tron.ex
+++ lib/cryptopunk/crypto/tron.ex
@@ -1,41 +1,58 @@
-defmodule Cryptopunk.Crypto.Ethereum do
+defmodule Cryptopunk.Crypto.Tron do
   @moduledoc """
-  Ethereum address generation logic
+  Tron address generation logic.
   """
+
   alias Cryptopunk.Key
 
+  @version_bytes %{
+    mainnet: <<0x41>>,
+    testnet: <<0xA0>>
+  }
+
   @doc """
-  Generate an ethereum address. EVM compatible chains (for example Binance Smart Chain) may use the same address format.
+  Generate a tron address.
+
+  It accepts two parameters:
+
+  - public or private key. if a private key is provided, it will be converted to public key.
+  - network (`:mainnet` or `:testnet`)
 
   Examples:
 
       iex> private_key = %Cryptopunk.Key{key: <<16, 42, 130, 92, 247, 244, 62, 96, 24, 129, 187, 141, 124, 42, 176, 116, 234, 171, 184, 107, 3, 229, 255, 72, 30, 116, 79, 243, 36, 142, 184, 24>>, type: :private}
-      iex> Cryptopunk.Crypto.Ethereum.address(private_key)
-      "0x74510e5055179bcf406d0e7449b47dee9f81e8b7"
+      iex> Cryptopunk.Crypto.Tron.address(private_key, :mainnet)
+      "TLaEVB6foF9nPn4sqscXc5jeH1PVyLwwFK"
 
       iex> public_key = %Cryptopunk.Key{key: <<4, 57, 163, 96, 19, 48, 21, 151, 218, 239, 65, 251, 229, 147, 160, 44, 197, 19, 208, 181, 85, 39, 236, 45, 241, 5, 14, 46, 143, 244, 156, 133, 194, 60, 190, 125, 237, 14, 124, 230, 165, 148, 137, 107, 143, 98, 136, 143, 219, 197, 200, 130, 19, 5, 226, 234, 66, 191, 1, 227, 115, 0, 17, 98, 129>>, type: :public}
-      iex> Cryptopunk.Crypto.Ethereum.address(public_key)
-      "0x056db290f8ba3250ca64a45d16284d04bc6f5fbf"
+      iex> Cryptopunk.Crypto.Tron.address(public_key, :testnet)
+      "27PaDLFYbGN9ztPPT3YiTatLQEdEJ2xsfWM"
   """
-  @spec address(Key.t()) :: String.t()
-  def address(%Key{type: :private} = private_key) do
+  @spec address(Key.t(), atom()) :: String.t()
+  def address(key, network \\ :mainnet)
+
+  def address(%Key{type: :private} = private_key, network) do
     private_key
     |> Key.public_from_private()
-    |> address()
+    |> address(network)
   end
 
-  def address(%Key{type: :public} = public_key) do
+  def address(%Key{type: :public} = public_key, network) do
     public_key
     |> pub_key_64_bytes()
     |> hash_256()
     |> get_last_20_bytes()
-    |> to_address()
+    |> to_address(network)
   end
 
-  defp to_address(public_hash) do
-    address = Base.encode16(public_hash, case: :lower)
+  defp to_address(hash, network) do
+    {:ok, encoded} =
+      @version_bytes
+      |> Map.fetch!(network)
+      |> Kernel.<>(hash)
+      |> ExBase58.encode_check()
 
-    "0x" <> address
+    encoded
   end
 
   defp hash_256(data) do
---
+++ test/cryptopunk/crypto/tron_test.exs
@@ -0,0 +1,57 @@
+defmodule Cryptopunk.Crypto.TronTest do
+  use ExUnit.Case
+
+  alias Cryptopunk.Crypto.Tron
+  alias Cryptopunk.Key
+
+  doctest Tron
+
+  test "generates the same address from public and private keys" do
+    private_key = %Cryptopunk.Key{
+      chain_code:
+        <<153, 249, 145, 92, 65, 77, 50, 249, 120, 90, 178, 30, 41, 27, 73, 128, 74, 201, 91, 250,
+          143, 238, 129, 247, 115, 87, 161, 107, 123, 63, 84, 243>>,
+      key:
+        <<50, 8, 92, 222, 223, 155, 132, 50, 53, 227, 114, 79, 88, 11, 248, 24, 239, 76, 236, 39,
+          195, 198, 112, 133, 224, 41, 65, 138, 91, 47, 111, 43>>,
+      type: :private,
+      depth: 0,
+      parent_fingerprint: <<0, 0, 0, 0>>,
+      index: 0
+    }
+
+    public_key = Key.public_from_private(private_key)
+
+    address_from_private_key = Tron.address(private_key)
+    address_from_public_key = Tron.address(public_key)
+
+    assert address_from_private_key == address_from_public_key
+    assert "TJmuGjyJVmbPVyxuVsS9G2Uio5aEWvWLtb" == address_from_private_key
+  end
+
+  test "generates correct mainnet addresses from derivation path" do
+    mnemonic =
+      "gospel bitter ladder chimney critic glory decorate exist build rescue phrase good dust harbor tourist"
+
+    seed = Cryptopunk.create_seed(mnemonic)
+    master_key = Cryptopunk.master_key_from_seed(seed)
+    base_path = "m/44'/195'/0'/0"
+
+    expected_addresses = %{
+      0 => "TLWMdArGeBCE3ktiu165w7Tiu2C2JEnW7p",
+      1 => "TQ79zY3LC9bGAD9htj9mz8q9wkmKzpgCN3",
+      2 => "TUYAGCna6UQZ7ogLCrQ3uz33fTsez9hKpo",
+      3 => "TVoJtzDWT57sHJa9gy4q6CpMnDngDze3Kd",
+      4 => "TELVvau4r1TkeWfZBYHPmmHCEdSv5PBzwZ",
+      5 => "TLaybyfiyhDrXNwVf1P57bTxxksadEvmGZ"
+    }
+
+    Enum.each(expected_addresses, fn {idx, expected_address} ->
+      {:ok, path} = Cryptopunk.parse_path("#{base_path}/#{idx}")
+
+      key = Cryptopunk.derive_key(master_key, path)
+
+      assert expected_address == Tron.address(key)
+    end)
+  end
+end
