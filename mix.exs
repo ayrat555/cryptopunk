6a42ac41e42a76a74e59d66e9e6f3f5f222bd927
Ayrat Badykov <ayratin555@gmail.com>
2022-09-01 09:48:01 +0300

    update deps

--- mix.exs
+++ mix.exs
@@ -26,10 +26,10 @@ defmodule Cryptopunk.MixProject do
   defp deps do
     [
       {:ex_keccak, "~> 0.4"},
-      {:ex_pbkdf2, "~> 0.5"},
+      {:ex_pbkdf2, "~> 0.6"},
       {:ex_secp256k1, "~> 0.5"},
-      {:ex_bech32, "~> 0.2"},
-      {:ex_base58, "~> 0.3"},
+      {:ex_bech32, "~> 0.3"},
+      {:ex_base58, "~> 0.4"},
       {:jason, "~> 1.3"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
