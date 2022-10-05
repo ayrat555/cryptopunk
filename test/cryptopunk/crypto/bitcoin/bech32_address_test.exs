78a8738e59d35b91988e0ba1f640a69eeb0920d7
Ayrat Badykov <ayratin555@gmail.com>
2022-10-05 19:27:20 +0300

    fix test

--- test/cryptopunk/crypto/bitcoin/bech32_address_test.exs
+++ test/cryptopunk/crypto/bitcoin/bech32_address_test.exs
@@ -6,7 +6,9 @@ defmodule Cryptopunk.Crypto.Bitcoin.Bech32AddressTest do
   describe "validate/1" do
     test "validates a taproot address (mainnet)" do
       assert {:ok, %{network: :mainnet, type: :p2tr}} =
-               Bech32Address.validate("bc1pmzfrwwndsqmk5yh69yjr5lfgfg4ev8c0tsc06e")
+               Bech32Address.validate(
+                 "bc1p53tapspendyea5kl2uwkz2a9fxh87xvnslkuatqhtxvjzrm25wwsktfwpg"
+               )
     end
 
     test "validates a p2wpkh address (mainnet)" do
@@ -17,6 +19,11 @@ defmodule Cryptopunk.Crypto.Bitcoin.Bech32AddressTest do
     test "validates a p2sh (mainnet)" do
       {:ok, %{network: :mainnet, type: :p2wsh}} =
         Bech32Address.validate("bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3")
+    end
+
+    test "validates a p2pkh (regtest)" do
+      assert {:ok, %{network: :regtest, type: :p2wpkh}} =
+               Bech32Address.validate("bcrt1qzldwmjeadqj4qllx985csduem3m6ajn0uzgepp")
     end
   end
 end
