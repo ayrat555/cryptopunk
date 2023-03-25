722028aaa2b0ea4dddd263bd494c8bf79ceae854
Ayrat Badykov <ayratin555@gmail.com>
2023-03-25 16:05:02 +0200

    update mnemoniac

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cryptopunk.MixProject do
     [
       app: :cryptopunk,
       version: "0.6.7",
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Hierarchical deterministic wallet (HD Wallet)",
@@ -30,7 +30,7 @@ defmodule Cryptopunk.MixProject do
       {:ex_secp256k1, "~> 0.7"},
       {:ex_bech32, "~> 0.4"},
       {:ex_base58, "~> 0.5"},
-      {:mnemoniac, "~> 0.1.0"},
+      {:mnemoniac, "~> 0.1.1"},
       {:jason, "~> 1.3"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
