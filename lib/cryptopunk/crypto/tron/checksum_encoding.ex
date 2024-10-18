02230b6b5e5da82bea18197618c97b1752e3c9cd
pxp9 <pepe.marquezromero@protonmail.com>
2024-10-18 10:29:37 +0300

    some fixes but still is not working

--- lib/cryptopunk/crypto/tron/checksum_encoding.ex
+++ lib/cryptopunk/crypto/tron/checksum_encoding.ex
@@ -10,24 +10,27 @@ defmodule Cryptopunk.Crypto.Tron.ChecksumEncoding do
   end
 
   defp do_check_address_checksum(address) do
-    with {:ok, binary} <-
+    with {:ok, bin} <-
            ExBase58.decode(address) do
-      <<address_without_checksum::binary-size(byte_size(binary) - 4), checksum::binary-size(4)>> =
-        binary
+      <<address_without_checksum::binary-size(byte_size(bin) - 4), checksum::binary-size(4)>> =
+        bin
 
       checksum = checksum |> Base.encode16(case: :lower)
 
       double_hash =
         address_without_checksum
         |> Base.encode16(case: :lower)
-        |> ExKeccak.hash_256()
-        |> ExKeccak.hash_256()
 
-      <<expected_hash::binary-size(8), _::binary>> = double_hash
+      double_hash =
+        :crypto.hash(:sha3_256, double_hash)
+        |> Base.encode16(case: :lower)
 
-      IO.inspect(expected_hash |> Base.encode16(case: :lower))
+      double_hash =
+        :crypto.hash(:sha3_256, double_hash)
 
-      IO.inspect(checksum)
+      <<expected_hash::binary-size(4), _::binary>> = double_hash
+
+      IO.inspect("#{expected_hash |> Base.encode16(case: :lower)} == #{checksum}")
     else
       {:error, :invalid_alphabet} -> {:error, :invalid_alphabet}
       {:error, :decode_error} -> {:error, :decode_error}
