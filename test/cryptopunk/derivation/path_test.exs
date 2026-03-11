20b6d95915e51d8209db68fe07096f92f124d875
Ayrat Badykov <ayratin555@gmail.com>
2026-03-11 10:02:58 +0200

    Parse incomplete paths (#105)

--- test/cryptopunk/derivation/path_test.exs
+++ test/cryptopunk/derivation/path_test.exs
@@ -44,4 +44,42 @@ defmodule Cryptopunk.Derivation.PathTest do
                Path.to_raw_path(path)
     end
   end
+
+  describe "parse_incomplete_path/1" do
+    test "parsed an incomplete path" do
+      path = "M/100/99'/10'/9/1000'"
+
+      assert {:ok,
+              {
+                :public,
+                [
+                  100,
+                  Path.two_power_31() + 99,
+                  Path.two_power_31() + 10,
+                  9,
+                  Path.two_power_31() + 1000
+                ]
+              }} ==
+               Path.parse_incomplete_path(path)
+    end
+
+    test "fails to parth path if type is invalid" do
+      path = "T/99/11"
+
+      assert {:error, {:invalid_level, :type}} ==
+               Path.parse_incomplete_path(path)
+    end
+
+    test "parse incomplete regular paths" do
+      paths = [
+        {"M/84'/0'/0'", {:public, [2_147_483_732, 2_147_483_648, 2_147_483_648]}},
+        {"M/49'/0'/0'", {:public, [2_147_483_697, 2_147_483_648, 2_147_483_648]}},
+        {"M/44'/0'/0'", {:public, [2_147_483_692, 2_147_483_648, 2_147_483_648]}}
+      ]
+
+      Enum.each(paths, fn {str_path, expected} ->
+        assert {:ok, expected} == Path.parse_incomplete_path(str_path)
+      end)
+    end
+  end
 end
